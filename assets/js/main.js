abbacaa61766f94c2bdc7f47b345ef847a587544
ChristianSangyy <chrisanchieta23@gmail.com>
2026-01-14 01:18:23 -0300

    alteraçoes na area de experiencia

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,6 @@ function updateProfileInfo(profileData) {
     const name = document.getElementById('profile.name')
     name.innerText = profileData.name
 
-    const job = document.getElementById('profile.job')
-    job.innerText = profileData.job
-
     const location = document.getElementById('profile.location')
     location.innerText = profileData.location
 
@@ -55,7 +52,6 @@ function updateProfessionalExperience(profileData) {
         return `
             <li>
                 <h3 class="title">${experience.name}</h3>
-                <p class="period">${experience.period}</p>
                 <p>${experience.description}</p>
             </li>
         `
